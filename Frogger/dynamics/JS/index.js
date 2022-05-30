81395fdfde950dee3dfc2910f3d2e6da22e6d018
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-30 14:00:25 -0500

    Listo

--- Frogger/dynamics/JS/index.js
+++ Frogger/dynamics/JS/index.js
@@ -1,10 +1,34 @@
 const canvas = document.getElementById("mi-canvas");
 const ctx = canvas.getContext("2d");
+
+const naveEspacial = new Image(100, 100);
+naveEspacial.src = "./statics/media/img/avion.png";
+naveEspacial.addEventListener('load', ()=>{
+    console.log("Ya cargó");
+    ctx.drawImage(naveEspacial, 225, 225, 50, 45);
+})
 // const up = document.getElementById("up");
 // const left = document.getElementById("left");
 // const right = document.getElementById("right");
 // const down = document.getElementById("down");
 
+// class spaceCraft
+// {
+//     constructor(x, y, dx, dy, ruta)
+//     {
+//         const imagen = new image();
+//         imagen.src = ruta;
+//         this.x = x;
+//         this.y = y;
+//         this.dx = dx;
+//         this.dy = dy;
+//         this.spriteX = 0;
+//         this.spriteY = 0;
+//         this.img = imagen;
+//     }
+// }
+// const gamer = new spaceCraft(10, 20, 28, 28, "./statics/media/img/avion.png");
+// ctx.drawImage(gamer, 250, 250, 50, 45);
 
 let tempo = 5;
 let control = 0;
@@ -20,6 +44,9 @@ let naveOnU = 0;
 let naveOnL = 0;
 let naveOnR = 0;
 let naveOnD = 0;
+let vidas = 3;
+let pausa = 0;
+let juego;
 function static()
 {
     ctx.fillStyle = "#D398EB"
@@ -63,6 +90,10 @@ window.addEventListener("keypress", (evento) =>{
     {
         naveOnD = 1;
     }
+    if(evento.key == 32)
+    {
+        pausa = 1;
+    }
 });
 function nave()
 {
@@ -70,100 +101,107 @@ function nave()
     ctx.fillRect(xNave, yNave, 50, 45);
 }
 
-window.setInterval(()=>{
-    static();
-    // if(yNave <= canvas.height / 10 && xNave + 50 >= 71.42857142857143 && xNave + 50 <= 71.42857142857143 * 2 ||  yNave <= canvas.height / 10 && xNave >= 71.42857142857143 * 3 && xNave <= 71.42857142857143 * 4)
-    // {
-    //     yNave = canvas.height / 10;
-    // }
-    // Para que la nave no se pase de los límites del canvas
-    //Ha ganado
-    if(yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 && xNave <= canvas.width / 7 * 2 || yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 * 3 && xNave <= canvas.width / 7 * 4 || yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 * 5 && xNave <= canvas.width / 7 * 6)
-    {
-        yNave = canvas.height / 10;
-        //Sonido de ganador
-    }
-    if(yNave <= canvas.height / 10 * 2 && xNave + 50 >= 0 && xNave <= canvas.width / 7 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 2 && xNave <= canvas.width / 7 * 3 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 4 && xNave <= canvas.width / 7 * 5 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 6 && xNave <= canvas.width / 7 * 7)
-    {
-        yNave = canvas.height / 10 * 2;
-    }
-    if(yNave >= 453)
-    {
-        yNave = 453;
-    }
-    if(xNave <= 0)
-    {
-        xNave = 0;
-    }
-    if(xNave >= 500 - 50)
-    {
-        xNave = 500 - 50;
-    }
+juego = window.setInterval(()=>{
     
-    //Movimiento de nave
-    if(naveOnU == 1)
-    {
-        yNave = yNave - 8;
-        naveOnU = 0;
-    }
-    if(naveOnL == 1)
-    {
-        xNave = xNave - 8;
-        naveOnL = 0;
-    }
-    if(naveOnR == 1)
-    {
-        xNave = xNave + 8;
-        naveOnR = 0;
-    }
-    if(naveOnD == 1)
-    {
-        yNave = yNave + 8;
-        naveOnD = 0;
-    }
-    
-    nave();
-    //Obstáculos
-    ctx.fillStyle = "#666970";
-    ctx.fillRect(xUfo, yUfo, 50, 45);
-    xUfo = xUfo - 2;
-    if(roca == 1)
-    {
-        xRoca = xRoca - 2;
-        ctx.fillStyle = "#3BBB59";
-        ctx.fillRect(xRoca, yRoca, 50, 45);
-        if(xRoca == 0)
+        static();
+        // if(yNave <= canvas.height / 10 && xNave + 50 >= 71.42857142857143 && xNave + 50 <= 71.42857142857143 * 2 ||  yNave <= canvas.height / 10 && xNave >= 71.42857142857143 * 3 && xNave <= 71.42857142857143 * 4)
+        // {
+        //     yNave = canvas.height / 10;
+        // }
+        // Para que la nave no se pase de los límites del canvas
+        //Ha ganado
+        if(yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 && xNave <= canvas.width / 7 * 2 || yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 * 3 && xNave <= canvas.width / 7 * 4 || yNave <= canvas.height / 10 && xNave + 50 >= canvas.width / 7 * 5 && xNave <= canvas.width / 7 * 6)
         {
-            xRoca = 500;
-            yRoca = Math.random() * (415 - canvas.height / 10 * 2) + canvas.height / 10 * 2;
+            yNave = canvas.height / 10;
+            //Sonido de ganador
         }
-    }
-    //Cuando ufo llega al final
-    if(xUfo + 50 == 0)
-    {
-        xUfo = 500;
-        yUfo = Math.random() * (415 - canvas.height / 10 * 2) + canvas.height / 10 * 2;
-        control++;
-        //roca = 0;
-    }
-    //Cuando haya pasado 5 veces ufo de izquierda a derecha roca se activa
-    if(control == 5)
-    {
-        roca = 1;
-        //console.log(roca);        
-    }
-    if(control == 10)
-    {
-        control = 0;
-        roca = 0;
-    }
-    //Colisión con obstáculos
-    console.log(yUfo);
-    console.log(yNave);
-    if(yNave <= yUfo + 45 && yNave + 45 >= yUfo + 45)
-    {
-        ctx.fillStyle = "#F54936";
-        ctx.fillRect(225, 225, 50, 50);
-    }
-
+        if(yNave <= canvas.height / 10 * 2 && xNave + 50 >= 0 && xNave <= canvas.width / 7 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 2 && xNave <= canvas.width / 7 * 3 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 4 && xNave <= canvas.width / 7 * 5 || yNave <= canvas.height / 10 * 2 && xNave + 50 >= canvas.width / 7 * 6 && xNave <= canvas.width / 7 * 7)
+        {
+            yNave = canvas.height / 10 * 2;
+        }
+        if(yNave >= 453)
+        {
+            yNave = 453;
+        }
+        if(xNave <= 0)
+        {
+            xNave = 0;
+        }
+        if(xNave >= 500 - 50)
+        {
+            xNave = 500 - 50;
+        }
+        
+        //Movimiento de nave
+        if(naveOnU == 1)
+        {
+            yNave = yNave - 8;
+            naveOnU = 0;
+        }
+        if(naveOnL == 1)
+        {
+            xNave = xNave - 8;
+            naveOnL = 0;
+        }
+        if(naveOnR == 1)
+        {
+            xNave = xNave + 8;
+            naveOnR = 0;
+        }
+        if(naveOnD == 1)
+        {
+            yNave = yNave + 8;
+            naveOnD = 0;
+        }
+        
+        nave();
+        //Obstáculos
+        ctx.fillStyle = "#666970";
+        ctx.fillRect(xUfo, yUfo, 50, 45);
+        xUfo = xUfo - 2;
+        if(roca == 1)
+        {
+            xRoca = xRoca - 2;
+            ctx.fillStyle = "#3BBB59";
+            ctx.fillRect(xRoca, yRoca, 50, 45);
+            if(xRoca == 0)
+            {
+                xRoca = 500;
+                yRoca = Math.random() * (415 - canvas.height / 10 * 2) + canvas.height / 10 * 2;
+            }
+        }
+        //Cuando ufo llega al final
+        if(xUfo + 50 == 0)
+        {
+            xUfo = 500;
+            yUfo = Math.random() * (415 - canvas.height / 10 * 2) + canvas.height / 10 * 2;
+            control++;
+            //roca = 0;
+        }
+        //Cuando haya pasado 5 veces ufo de izquierda a derecha roca se activa
+        if(control == 5)
+        {
+            roca = 1;
+            //console.log(roca);        
+        }
+        if(control == 10)
+        {
+            control = 0;
+            roca = 0;
+        }
+        //Colisión con obstáculos
+        console.log(yUfo);
+        console.log(yNave);
+        if(yNave <= yUfo + 45 && yNave + 45 >= yUfo + 45)
+        {
+            ctx.fillStyle = "#F54936";
+            ctx.fillRect(225, 225, 50, 50);
+            vidas--;
+            if(vidas == 0)
+            {
+                alert("Has perdido");
+                vidas = 3;
+            }
+        }
+    
 }, tempo);
